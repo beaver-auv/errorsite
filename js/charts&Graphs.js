4bd7d94927ef70c11d4a2d3e0a2ee59da4dae162
StarBlazed <bradleystone4531@gmail.com>
2025-03-03 12:12:25 -0500

    Desired Line
    Added a line that shows what PID the sub is aiming for

--- js/charts&Graphs.js
+++ js/charts&Graphs.js
@@ -1,7 +1,10 @@
 const motorGraph = document.getElementById("motorGraph");
-const chartPID = document.getElementById("chartPID")
-let timestamp = 0
-let AUVactive = true
+const chartPID = document.getElementById("chartPID");
+let timestamp = 0;
+let AUVactive = true;
+// Pull from same place as desiredPID (just for setup)
+let initialDesired = 20;
+let lastDesired = 20;
 
 let PID = new Chart(chartPID, {
   type: "line",
@@ -11,7 +14,17 @@ let PID = new Chart(chartPID, {
       label: "PID Level",
       data: [0],
       borderWidth: 1,
-      tension: 0.4
+      tension: 0.4,
+      backgroundColor: "#36A2EB",
+      borderColor: "#36A2EB"
+    },
+    {
+      label: "Desired PID",
+      data: [initialDesired],
+      borderColor: "black",
+      borderWidth: 1,
+      borderDash: [10, 5],
+      pointRadius: 0
     }]
   },
   options: {
@@ -24,7 +37,7 @@ let PID = new Chart(chartPID, {
           text: "Desired PID"
         },
         grid: {
-          color: 'rgba(0, 0, 0, 0.1)'
+          color: "rgba(0, 0, 0, 0.1)"
         }
       }
     }
@@ -33,12 +46,29 @@ let PID = new Chart(chartPID, {
 
 
 setInterval(function() {
-  //Fetch currentPID from other data
-  let currentPID = 0
-  PID.data.datasets[0].data.push(currentPID);
+  //Fetch the current PID level from other data later
+  let currentPID = 0;
+  PID.data.datasets[0].data.push(currentPID); 
 
-  timestamp -= 0.5
-  PID.data.labels.unshift(String(timestamp))
+  timestamp -= 0.5;
+  PID.data.labels.unshift(String(timestamp));
 
-  PID.update()
+  PID.update();
+
+  //Fetch the sub's desired PID from other data later
+  let desiredPID = 20;
+  PID.data.datasets[1].data.push(desiredPID);
+
+  if (timestamp == 0.5) {
+    PID.data.datasets[1].data.shift();
+    PID.data.datasets[1].data.push(desiredPID);
+  } else if (desiredPID != lastDesired) {
+    for (let i = 0; i <= PID.data.datasets[1].data.length; i++) {
+      PID.data.datasets[1].data[i] = desiredPID;
+    };
+  };
+
+  lastDesired = desiredPID;
+
+  PID.update("none");
 }, 500); // Executed every 500 miliseconds
