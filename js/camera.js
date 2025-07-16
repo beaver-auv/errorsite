f67f3b4dd2f51d1a5b7fd13e19cecd3170ca6a5d
StarBlazed <bradleystone4531@gmail.com>
2025-07-16 18:07:46 -0400

    Camera code proof of concept
    Efficively setting up the code for the camera without setting up the camera since I need a feed to save for testing.

--- js/camera.js
+++ js/camera.js
@@ -1,2 +1,10 @@
 /* Handles the view from the onboard camera */
 const cameraDisplay = document.getElementById("cameraDisplay")
+
+// Camera prototype
+// Uses computer camera since we don't have actual camera setup yet
+navigator.mediaDevices.getUserMedia({ video: true })
+  .then(stream => {
+    cameraDisplay.srcObject = stream;
+  })
+  .catch(err => errorHandler(`Camera error: ${err}`));
